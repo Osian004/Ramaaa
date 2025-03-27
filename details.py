1e1177d57553514ee60ce8e5a07a341aafa3398b
Ganesh-o9 <125977896+Ganesh-o9@users.noreply.github.com>
2025-03-27 14:30:01 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
 api_id = "20767676"
 api_hash = "7e1fd1a8ec48ad69175548350eae87b2"
-bot_token = "7722462379:AAFa_Ei5EYBP7JXeuJRMUZ452xybUHwKtHA"
-sudo_groups = -1004693152196
+bot_token = "6462557323:AAGhmSMvcrvxHo6IIev9D6COwitGZ6WGvsM"
+sudo_groups = -1002467329974
