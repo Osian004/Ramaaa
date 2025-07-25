efce358e42c76918c2695bfe444e0955e88023bb
Osian004 <ashokbana7865@gmail.com>
2025-07-25 21:34:47 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
 api_id = "20767676"
 api_hash = "7e1fd1a8ec48ad69175548350eae87b2"
-bot_token = "8011811111:AAFY6ICb3KMSclHeTlVBgILQyGUSFrictKs"
-sudo_groups = -1002751894880
+bot_token = "8347165504:AAFnX3aKWJ48QHWG4UcZMQM6knD6-KlWt7M"
+sudo_groups = --1002839853341
